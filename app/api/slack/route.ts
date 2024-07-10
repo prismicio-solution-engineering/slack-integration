1faf654043a95cd60a0cddf6d45a4bf64430cc3d
chamois-d-or <89452979+chamois-d-or@users.noreply.github.com>
2024-07-10 11:46:38 +0200

    update message

--- app/api/slack/route.ts
+++ app/api/slack/route.ts
@@ -1,86 +1,90 @@
 import * as prismicClient from '@prismicio/client'
 
 export async function POST(request: Request) {
-    try {
-        const slackUrl = process.env.SLACK_WEBHOOK_URL ? process.env.SLACK_WEBHOOK_URL : ""
-        const body = await request.json()
+  try {
+    const slackUrl = process.env.SLACK_WEBHOOK_URL ? process.env.SLACK_WEBHOOK_URL : ""
+    const body = await request.json()
 
-        const impactedDocs = body.documents
-        const client = prismicClient.createClient("slice-deck")
-        const publishedDocs = await client.getByIDs(impactedDocs)
-        const publishedDocsIds = publishedDocs.results.map(doc => doc.id)
-        const publishedDocsText = publishedDocsIds.length>0 ? "published decks : " + JSON.stringify(publishedDocsIds) + ", take a look here: https://slice-deck.prismic.io/builder/pages/" + publishedDocsIds[0]+ "?s=published\n" : ""
-        const unpublishedDocsIds = impactedDocs.filter((doc: string) => !publishedDocsIds.includes(doc))
-        const unpublishedDocsText = unpublishedDocsIds.length>0 ? "unpublished decks : " + JSON.stringify(unpublishedDocsIds) + ", take a look here: https://slice-deck.prismic.io/builder/pages/" + unpublishedDocsIds[0]+ "?s=archived\n" : ""
+    const impactedDocs = body.documents
+    const client = prismicClient.createClient("slice-deck")
+    const publishedDocs = await client.getByIDs(impactedDocs)
+    const publishedDocsResults = publishedDocs.results
+    const publishedDocsIds = publishedDocs.results.map(doc => doc.id)
+    const publishedDocsText = publishedDocsIds.length > 0 ? ":white_check_mark: published decks :\n" + publishedDocsResults.map(doc => JSON.stringify(doc.data.title + ", take a look here: https://slice-deck.prismic.io/builder/pages/" + doc.id )+ "?s=published\n").join('') : ""
+    const unpublishedDocsIds = impactedDocs.filter((doc: string) => !publishedDocsIds.includes(doc))
+    const unpublishedDocsText = unpublishedDocsIds.length > 0 ? ":x: unpublished decks :\n" + unpublishedDocsIds.map((id: string) => JSON.stringify(id) + ", take a look here: https://slice-deck.prismic.io/builder/pages/" + id + "?s=archived\n").join('') : ""
+    const fields = [
+      publishedDocsText &&
+      {
+        "type": "mrkdwn",
+        "text": publishedDocsText
+      },
+      unpublishedDocsText &&
+      {
+        "type": "mrkdwn",
+        "text": unpublishedDocsText
+      }
+    ]
 
-        const superText = {
-            "blocks": [
-              {
-                "type": "section",
-                "text": {
-                  "type": "mrkdwn",
-                  "text": ":mega: *Document Updates* :mega:"
-                }
-              },
-              {
-                "type": "divider"
-              },
-              {
-                "type": "section",
-                "fields": [
-                  {
-                    "type": "mrkdwn",
-                    "text": "*Published Document:*"
-                  },
-                  {
-                    "type": "mrkdwn",
-                    "text": "*Unpublished Document:*"
-                  }
-                ]
-              },
-              {
-                "type": "section",
-                "fields": [
-                  {
-                    "type": "mrkdwn",
-                    "text": ":white_check_mark: " + publishedDocsText
-                  },
-                  {
-                    "type": "mrkdwn",
-                    "text": ":x: " + unpublishedDocsText
-                  }
-                ]
-              },
-              {
-                "type": "divider"
-              },
-              {
-                "type": "context",
-                "elements": [
-                  {
-                    "type": "mrkdwn",
-                    "text": "For more details, please check the document management system."
-                  }
-                ]
-              }
-            ]
+    const superText = {
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":cool-dog: *Document Updates* :cool-dog:"
           }
-
-        const res = await fetch(slackUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(superText
-            ),
-        })
-        const status = res.status
-        console?.log(status)
-        if (status === 200) {
-            return Response.json({ "status": "ok" })
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "fields": fields
+        },
+        // {
+        //   "type": "section",
+        //   "fields": [
+        //     {
+        //       "type": "mrkdwn",
+        //       "text": publishedDocsText
+        //     },
+        //     {
+        //       "type": "mrkdwn",
+        //       "text": unpublishedDocsText
+        //     }
+        //   ]
+        // },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "mrkdwn",
+              "text": "For more details, please check the document management system: https://slice-deck.prismic.io."
+            }
+          ]
         }
-    } catch (error) {
-        console.log(error)
+      ]
     }
-    return Response.json({})
+
+    const res = await fetch(slackUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(superText
+      ),
+    })
+    const status = res.status
+    console?.log(status)
+    if (status === 200) {
+      return Response.json({ "status": "ok" })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  return Response.json({})
 }
